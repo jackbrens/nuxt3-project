6b3d3fc3ca8830cb9dcf970dda1364d303fca2ff
JackBrens <574950259@qq.com>
2023-02-20 21:42:41 +0800

    feat: 🚀 1.新增eslint校验块注释之前空行 2. setting.json 添加保存自动修复

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,15 +13,34 @@ module.exports = {
 	rules: {
 		'no-undef': 0,
 		'vue/multi-word-component-names': 0,
+
 		// 关闭非驼峰命名法提示错误
 		camelcase: 0,
+
 		// 末尾不要分号
 		semi: 2,
+
 		// 单引号
 		quotes: 2,
+
+		// 不使用 var 声明变量
 		'no-var': 2,
+		'lines-around-comment': [
+			2,
+			{
+				beforeBlockComment: true, // 块注释之前空行
+				beforeLineComment: true, // 行注释之前空行
+				allowBlockStart: true, // 允许注释出现在块语句的开始位置
+				allowObjectStart: true, // 允许注释出现在对象字面量的开始位置
+				allowArrayStart: true, // 允许注释出现在数组字面量的开始位置
+				allowClassStart: true, // 允许注释出现在类的开始位置
+				ignorePattern: '#',
+			},
+		],
+
 		// 强制数组方括号中使用一致的空格
 		'array-bracket-spacing': 2,
+
 		// 强制对象中使用一致的空格
 		'object-curly-spacing': 2,
 	},
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
 			name: 'Nuxt3-project', // 设置启动项目名称
 			exec_mode: 'cluster',
 			instances: 'max',
+
 			// 注意这里的相对路径。要访问到index.mjs就行了，如果你是整个.output一起放在服务器的话就和官方一样路径前面加一个/.output好了
 			script: '/server/index.mjs',
 		},
